6bbc111c29def60a534f30e05d46fc14dc5ee4f1
Ocuos <93371564+Ocuos@users.noreply.github.com>
2025-04-18 23:01:55 +0700

    Create insert_sample_data.sql

---
+++ insert_sample_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO User (UserID, Name, Email, Phone)
+VALUES 
+    (1, 'Linh', 'linh@email.com', '0123456789'),
+    (2, 'Huy', 'huy@email.com', '0987654321');
+
+INSERT INTO Court (CourtID, Name, Location, Description)
+VALUES
+    (1, 'Hà Nội A', 'Hà Nội', 'Outdoor court with lights');
