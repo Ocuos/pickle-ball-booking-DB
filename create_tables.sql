9d6ba32eeb3c85e64a65fd2a6919279be47cab58
Ocuos <93371564+Ocuos@users.noreply.github.com>
2025-04-18 23:00:36 +0700

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -4,3 +4,11 @@ CREATE TABLE User (
     Email TEXT UNIQUE NOT NULL,
     Phone TEXT
 );
+
+CREATE TABLE Court (
+    CourtID INTEGER PRIMARY KEY,
+    Name TEXT NOT NULL,
+    Location TEXT NOT NULL,
+    Description TEXT
+);
+-- And so on for TimeSlot and Booking
